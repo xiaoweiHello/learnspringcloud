05f38544886e3f1c75345ba1964160ba7bb8686c
weizheng <xiaowei_hello@126.com>
2019-07-25 18:26:07 +0800

    1. 添加配置中心服务 2. 添加服务注册中心集群

---
+++ service-zipkin/src/main/java/com/xiaowei/servicezipkin/ServiceZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.xiaowei.servicezipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZipkinApplication.class, args);
+	}
+
+}
